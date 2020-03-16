90b2cebd9606c8f181fc90369173fa19a8077a91
Vidar H <vusvol@icloud.com>
2020-03-16 12:10:54 +0100

    Added answer to question 1 level 2.

---
+++ level-2.js
@@ -0,0 +1,9 @@
+// Course assignment | Programming Foundations | Vidar Håland
+// LEVEL 2
+
+// 1. Using the loop from question 5 above, only log the counter variable if it is an even number.
+for (var counter = 15; counter < 26; counter++) {
+  if (counter % 2 === 0) {
+    console.log(counter);
+  }
+}
