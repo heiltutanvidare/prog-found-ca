9b6f13fa11265d3b66141684e6fd5458d8057a9d
Vidar H <vusvol@icloud.com>
2020-03-16 10:55:36 +0100

    Added answers to question 5, 6 and 7.

--- level-1.js
+++ level-1.js
@@ -28,13 +28,40 @@ for (var i = 0; i < numberArray.length; i++) {
   console.log(numberArray[i]);
 }
 // 5. Create a for loop that counts from 15 to 25. Console log the value of the counter variable inside the loop.
+for (var counter = 15; counter < 26; counter++) {
+  console.log(counter);
+}
 
 // 6. Using the same loop from the above question, only log the value of the counter variable if it is equal to 20.
+for (var counter = 15; counter < 26; counter++) {
+  if (counter === 20) {
+    console.log(counter);
+  }
+}
+
 // 7. Create an array of two objects. Each object must have the same three properties (with different values):
 // 7. one property with a string value
 // 7. one property with a number value
 // 7. one property with a boolean value
 // 7. Loop through the array and console log the number value and the boolean value.
+var cars = [
+  {
+    make: "Toyota",
+    productionYear: 2001,
+    isFast: true
+  },
+  {
+    make: "Mazda",
+    productionYear: 1993,
+    isFast: false
+  }
+];
+
+for (var i = 0; i < cars.length; i++) {
+  console.log(cars[i].productionYear);
+  console.log(cars[i].isFast);
+}
+
 // 8. Create a function called whatIDontLike that accepts one argument. Choose a good name for the argument.
 // 8. Inside the function, log the string "I don't like " together with the argument.
 // 8. Call the function and pass in a value of your choice.
