3a3a6e39b5d9f19e4d8e9ea406e7af284fd850ab
Vidar H <vusvol@icloud.com>
2020-03-16 10:29:29 +0100

    Added all questions in comments to the file. Making it ready for answers.

---
+++ level-1.js
@@ -0,0 +1,24 @@
+// Course assignment | Programming Foundations
+// LEVEL 1
+// 1. Declare and initialise a variable with a string value.
+// 2. Create an object variable called person and give it two properties (a key and a value) of your choice.
+// 3. Create a variable called outOfStock and assign it a boolean value.
+// 3. Create an if else statement that checks the value of outOfStock.
+// 3. If it is true, console log "Out of stock". Otherwise log "In stock".
+// 4. Create an array of five numbers. Loop through the array and console log each value.
+// 5. Create a for loop that counts from 15 to 25. Console log the value of the counter variable inside the loop.
+// 6. Using the same loop from the above question, only log the value of the counter variable if it is equal to 20.
+// 7. Create an array of two objects. Each object must have the same three properties (with different values):
+// 7. one property with a string value
+// 7. one property with a number value
+// 7. one property with a boolean value
+// 7. Loop through the array and console log the number value and the boolean value.
+// 8. Create a function called whatIDontLike that accepts one argument. Choose a good name for the argument.
+// 8. Inside the function, log the string "I don't like " together with the argument.
+// 8. Call the function and pass in a value of your choice.
+// 9. Create a function that accepts two arguments.
+// 9. Inside the function, subtract the second argument from the first and console log the result.
+// 10. Create an empty array.
+// 10. Create a function that accepts one argument.
+// 10. Inside the function, add the argument to the array.
+// 10. Call the function and pass in a value of any type.
